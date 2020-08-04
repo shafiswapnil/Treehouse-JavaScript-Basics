047a86331eca44eb4cbf7f01a5624c314e862e9b
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-08-04 14:49:30 +0600

    challenge solved

---
+++ 00-Intro/02-Debugging/script.js
@@ -0,0 +1,17 @@
+console.log("Program start");
+
+// Problem to be fixed
+/*
+alert("Help me fix this program!");
+alert(Can you get this message to appear ?);
+document.write("<h2>My first JavaScript program</h2>");
+document.wrong("<p>I'm practicing 'debugging'.</p>");
+*/
+
+// Solution
+alert("Help me fix this program!");
+alert("Can you get this message to appear ?");
+document.write("<h2>My first JavaScript program</h2>");
+document.write("<p>I'm practicing 'debugging'.</p>");
+
+console.log("Program finised");
