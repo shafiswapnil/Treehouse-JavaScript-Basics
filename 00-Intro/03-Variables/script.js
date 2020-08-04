931eea72638781420224e304593cc46086c6d77f
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-08-04 15:50:00 +0600

    added variable naming rules

--- 00-Intro/03-Variables/script.js
+++ 00-Intro/03-Variables/script.js
@@ -1,7 +1,11 @@
 // Variables
 
 // USE MEANINGFUL name for variables
-// 
+// javascript has a long list of reserved words. so don't use reserved keywords
+// name can't start with a number!
+// number, letter, dollar, underscare
+// camelCase pricePerUnit
+// provide descriptive variable name
 
 var message;
 // var message = "Hello!";
