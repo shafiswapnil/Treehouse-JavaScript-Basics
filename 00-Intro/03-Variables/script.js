f79f9413ea78043b3933d458d39182f1edf96cf3
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-08-04 15:41:03 +0600

    js variables (basics)

---
+++ 00-Intro/03-Variables/script.js
@@ -0,0 +1,10 @@
+// Variables
+
+// USE MEANINGFUL name for variables
+// 
+
+var message;
+// var message = "Hello!";
+message = "Hello!";
+message = "Welcome to JavaScript Basics"
+alert(message);
