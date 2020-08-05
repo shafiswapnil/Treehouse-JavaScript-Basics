9f17ade26843b4937cd8ae31ad0d37e3e1736655
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-08-06 01:31:09 +0600

    remade the story making program

---
+++ 01-Numbers/02-StoryMaker-v2/script.js
@@ -0,0 +1,26 @@
+var questions = 3;
+var questionsLeft = " [" + questions + " questions left]";
+
+var adjective = prompt("Type an adjective" + questionsLeft);
+questions -= 1;
+
+var questionsLeft = " [" + questions + " questions left]";
+var verb = prompt("Type a verb" + questionsLeft);
+questions -= 1;
+
+var questionsLeft = " [" + questions + " questions left]";
+var noun = prompt("Type a noun" + questionsLeft);
+questions -= 1;
+
+window.alert("Ready for the story?");
+
+var story;
+story = "There once a ";
+story += adjective;
+story += " programmer who wanted to use JavaScript to "
+story += verb;
+story += " the world."
+
+document.getElementById("story").innerHTML = story;
+console.log(story);
+console.log(questions);
