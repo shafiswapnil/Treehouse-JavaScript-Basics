bab4bc30098087092544f6c51050c3fe3882d5e9
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-08-06 02:04:16 +0600

    learned and tested Math object

---
+++ 01-Numbers/03-Math-Object/script.js
@@ -0,0 +1,20 @@
+// run node script.js in terminal to see results.
+// please refer to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math to know more
+
+console.log(Math.round(2.9));
+console.log(Math.ceil(1.3));
+console.log(Math.floor(1.9));
+console.log(Math.ceil(3.21));
+console.log(Math.abs(-1));
+console.log(Math.abs('-4'));
+console.log(Math.PI);
+// Math.cos(radians) * radius
+console.log(Math.cos(1) * 10);
+console.log(Math.max(1, 2, 3, 4));
+console.log(Math.max(1, 0, -3, -4));
+console.log(Math.pow(2, 2));
+console.log(Math.pow(5, 0.2));
+console.log(Math.pow(5, -1));
+console.log(Math.sign(466));
+console.log(Math.sign(-5));
+console.log(Math.sign(0));
