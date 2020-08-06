84fde86f2c94046bf3e71b3516a0b373e81f552e
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-08-06 06:07:48 +0600

    solution to random number generator (final)

--- 01-Numbers/05-Random-Number-Generator/script.js
+++ 01-Numbers/05-Random-Number-Generator/script.js
@@ -5,7 +5,10 @@ var num1 = parseInt(first);
 var num2 = parseInt(second);
 
 var randomNum1 = Math.floor(Math.random() * num1) + 1;
-var randomNum2 = Math.floor(Math.random() * num1) + num2;
+var randomNum2 = Math.floor(Math.random() * (num1 - num2 + 1)) + num2;
 
-document.getElementById("random1").innerHTML = randomNum1;
-document.getElementById("random2").innerHTML = randomNum2;
+var message1 = randomNum1 + " in between 0 and " + num1;
+var message2 = randomNum2 + " in between " + num1 + " and " + num2;
+
+document.getElementById("random1").innerHTML = message1;
+document.getElementById("random2").innerHTML = message2;
