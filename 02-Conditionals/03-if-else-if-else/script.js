5948469781d2c615a8fcb9f70c330388b6cc2170
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-08-06 23:52:26 +0600

    added comments in script file (if else else if)

--- 02-Conditionals/03-if-else-if-else/script.js
+++ 02-Conditionals/03-if-else-if-else/script.js
@@ -1,10 +1,28 @@
+/*
+ The Random Number Guessing Game
+ Generates a number between 1 and 6
+ and gives a player two attempts to
+ guess the number.
+ */
+
+// assume the player didn't guess correctly
 var correntGuess = false;
+
+// generates random number from 1 to 6
 var randomNumber = Math.floor(Math.random() * 6) + 1;
+
 var guess = prompt("I'm thinking a number between 0 and 6. What is it?");
 
+// targetting feedback id in html
 var feedback = document.getElementById("feedback");
+// parsing string to number
 var guess = parseInt(guess);
 
+/* test to see if player is
+1. correct
+2. guessed too high
+3. guessed too low
+*/
 if (guess === randomNumber) {
 
 	correntGuess = true;
@@ -21,6 +39,7 @@ if (guess === randomNumber) {
 
 }
 
+// test if player is correct and output response
 if (correntGuess) {
 
 	feedback.innerHTML = "You guessed the number!";
