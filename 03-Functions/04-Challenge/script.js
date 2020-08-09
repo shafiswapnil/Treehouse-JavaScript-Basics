b1976d21082c61dbe8280d7ef95c1da2f026d360
Shafi_Swapnil <shafiahmedswapnil6494@gmail.com>
2020-08-09 19:50:02 +0600

    made the random number generator function more responsive

--- 03-Functions/04-Challenge/script.js
+++ 03-Functions/04-Challenge/script.js
@@ -1,17 +1,27 @@
+// Prompting the user for a lower and an upper value
 var lower = prompt("Enter a lower value");
 var upper = prompt("Enter a upper value");
 
+// Parsing the input to integers
 var lower = parseInt(lower);
 var upper = parseInt(upper);
 
+// Random number generator function
 function getRanomNumber(lower, upper) {
+	// Check if lower or upper variable is not a number,
+	// if true, throw an error!
+	if (isNaN(lower) || isNaN(upper)) {
+		throw new Error("Both arguments must be numbers!");
+	}
+
+	// Directly returning the generated random number
 	return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 }
 
+// Storing the random number from the function
 var randomNumber = getRanomNumber(lower, upper);
 
-// output stuff
+// It's output time
 document.getElementById("upper").innerHTML = upper;
 document.getElementById("lower").innerHTML = lower;
 document.getElementById("result").innerHTML = randomNumber;
-
